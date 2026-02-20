753f4eecff9eb432d26cf2a190fcf6da2ab27c5b
LaurenMurillo <murillolauren62@gmail.com>
2026-02-19 23:39:02 -0600

    //@param your average for a class on homework, quizzes, and exams, respectively //@return the value of your overall grade in the class function gradeCalculator(homeworkAvg, quizAvg, examAvg) {     let homeworkWeight = .10;     let quizWeight = .15;     let examWeight = .75;     //calculations:     let totalPoints = (homeworkAvg * homeworkWeight) + (quizAvg * quizWeight) + (examAvg * examWeight)     let categories = 3;     return totalPoints / categories; }

--- Week2/week2-demo.js
+++ Week2/week2-demo.js
@@ -29,3 +29,32 @@ function cylinderVolume(radius, height){
 
 console.log(cylinderVolume(radius, height));
 //_name means it is "private"
+
+//hw from slide 33: (run code --> node [fileName.fileType])
+
+//@param side lengths of two different squares
+//@return the sum of the areas of these squares
+function sumOfSquareAreas(a, b) {
+    let areaOne = a ** 2;
+    let areaTwo = b ** 2;
+    return areaOne + areaTwo;
+}
+
+console.log(sumOfSquareAreas(9, 3));//90
+console.log(sumOfSquareAreas(5, 10));//125
+console.log(sumOfSquareAreas(4, 6));//52
+
+//@param your average for a class on homework, quizzes, and exams, respectively
+//@return the value of your overall grade in the class
+function gradeCalculator(homeworkAvg, quizAvg, examAvg) {
+    let homeworkWeight = .10;
+    let quizWeight = .15;
+    let examWeight = .75;
+    //calculations:
+    let totalPoints = (homeworkAvg * homeworkWeight) + (quizAvg * quizWeight) + (examAvg * examWeight)
+    return totalPoints;
+}
+
+console.log(gradeCalculator(100, 85, 90));//90
+console.log(gradeCalculator(94, 78, 92));//125
+console.log(gradeCalculator(85, 90, 95));//52
